12ca61e423cb1b0453dc51f2fa2082812969e932
vidun-jay <69271445+vidun-jay@users.noreply.github.com>
2023-06-05 21:33:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 resource "aws_instance" "my-first-server" {
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
-		tags = {
-			Name = "ubuntu"
-		}
+    tags = {
+	Name = "ubuntu"
+    }
 }
